a8b51826a631ed3f0bd8a44ac07301a9e8cee89a
adam <adamdiniz52@gmail.com>
2026-09-04 20:38:24 -0300

    faça isso

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Pressione uma tecla para continuar...");
+        System.Console.ReadKey();
+        System.Console.WriteLine();
+        System.Threading.Thread.Sleep(1000);
+        System.Console.WriteLine("Obrigado volte sempre");
